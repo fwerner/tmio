5c0c3851b06aec2d386e4273167468354344ed32
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2023-01-17 11:21:16 +0100

    Made static libraries explicit in linking step.

--- examples/Rules.mk
+++ examples/Rules.mk
@@ -1,4 +1,4 @@
 TGT_PREREQS  := lib/libtmio.a
 TGT_LDFLAGS  := -L${TARGET_DIR}/lib
-TGT_LDLIBS   := -ltmio -lbufio
+TGT_LDLIBS   := -l:libtmio.a -l:libbufio.a -lm
 SUBMAKEFILES += $(shell ls ${DIR}/tmio_*.mk | sed s,^${DIR}/,,g)

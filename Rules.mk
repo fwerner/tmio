1f6c65f2e3145ebb665b0f8c0152c6b2b5f19346
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2023-02-07 20:58:54 +0100

    Removed shared library building. Adapted linker flags to a more portable version. Added submodule initialization.

--- Rules.mk
+++ Rules.mk
@@ -1,3 +1,9 @@
 CFLAGS       := -std=c99 -O2 -Wall -Wextra -W -pedantic -march=native -fPIC
 INCDIRS      := src
-SUBMAKEFILES := src/Rules.mk src/Rules-shared.mk examples/Rules.mk
+SUBMAKEFILES := src/Rules.mk examples/Rules.mk
+
+.PHONY: submodules
+submodules:
+	git submodule update --init --recursive
+
+all: submodules
--- examples/Rules.mk
+++ examples/Rules.mk
@@ -1,4 +1,4 @@
 TGT_PREREQS  := lib/libtmio.a
 TGT_LDFLAGS  := -L${TARGET_DIR}/lib
-TGT_LDLIBS   := -l:libtmio.a -l:libbufio.a -lm
+TGT_LDLIBS   := -ltmio -lbufio -lm
 SUBMAKEFILES += $(shell ls ${DIR}/tmio_*.mk | sed s,^${DIR}/,,g)

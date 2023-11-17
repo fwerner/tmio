a4ed64eadb34bb3ec74b02951fac687c8563a799
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2023-11-17 17:04:22 +0100

    Add meson.

--- examples/tmio_example_writer.mk
+++
@@ -1,2 +0,0 @@
-TARGET      := bin/tmio_example_writer
-SOURCES     := tmio_example_writer.c
--- src/Rules-shared.mk
+++
@@ -1,6 +0,0 @@
-TARGET      := lib/libtmio.so
-SOURCES     := tmio.c
-SRC_INCDIRS := ../externals/bufio/src
-TGT_LDFLAGS := -shared -Llib
-TGT_LDLIBS  := -lbufio
-TGT_PREREQS := lib/libtmio.a
